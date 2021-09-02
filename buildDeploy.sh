b01adcfdf48ce314a8aabe0c3a2b0320844b823d
jlseto <setojs0@gmail.com>
2021-09-01 17:43:47 -0700

    asdf

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -3,6 +3,7 @@
 ng build --configuration production --output-path docs --base-href https://jimmyseto.com
 cp docs/index.html docs/404.html
 touch docs/.nojekyll
+echo "jimmyseto.com" docs/CNAME
 git add *
 git commit -am "updates"
 git push
