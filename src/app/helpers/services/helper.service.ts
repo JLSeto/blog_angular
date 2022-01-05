94bbb7dcc2ff7bc335f5ac6429c4deb38e053e60
jlseto <setojs0@gmail.com>
2022-01-04 21:57:32 -0800

    updates

--- src/app/helpers/services/helper.service.ts
+++ src/app/helpers/services/helper.service.ts
@@ -21,13 +21,17 @@ export class HelperService
 
   public checkifMobile() : void
   {
-    this.isMobile = (navigator.userAgent.match(/Android/i) 
-    || navigator.userAgent.match(/webOS/i) 
-    || navigator.userAgent.match(/iPhone/i)  
-    || navigator.userAgent.match(/iPad/i)  
-    || navigator.userAgent.match(/iPod/i) 
-    || navigator.userAgent.match(/BlackBerry/i) 
-    || navigator.userAgent.match(/Windows Phone/i)) ? true : false;
+    if(navigator.permissions)
+    {
+      this.isMobile = (navigator.userAgent.match(/Android/i) 
+      || navigator.userAgent.match(/webOS/i) 
+      || navigator.userAgent.match(/iPhone/i)  
+      || navigator.userAgent.match(/iPad/i)  
+      || navigator.userAgent.match(/iPod/i) 
+      || navigator.userAgent.match(/BlackBerry/i) 
+      || navigator.userAgent.match(/Windows Phone/i)) ? true : false;
+    }
+
   }
 
   public getIsMobile() : boolean
